bcdffb1622950d562096862ecc0503a08bff08a5
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2008-10-17 19:03:52 +0300

    Export Pattern from System.FilePath.Glob.

--- System/FilePath/Glob.hs
+++ System/FilePath/Glob.hs
@@ -1,11 +1,13 @@
 -- File created: 2008-10-10 13:37:42
 
 module System.FilePath.Glob
-   ( match
+   ( Pattern
+   , match
    , compile, tryCompile
    , globDir
    ) where
 
+import System.FilePath.Glob.Base      (Pattern)
 import System.FilePath.Glob.Compile   (compile, tryCompile)
 import System.FilePath.Glob.Directory (globDir)
 import System.FilePath.Glob.Match     (match)
